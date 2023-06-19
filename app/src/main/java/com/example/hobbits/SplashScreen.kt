7292044b4522c067eb611d241c190c6ca6ce35be
BryceMadelo <brycemadelo07@gmail.com>
2023-06-19 15:41:04 +0800

    Added Progress Bar

--- app/src/main/java/com/example/hobbits/SplashScreen.kt
+++ app/src/main/java/com/example/hobbits/SplashScreen.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ProgressBar
 
 class SplashScreen : AppCompatActivity() {
 
+    private lateinit var progressBar: ProgressBar
     private val Delay_Splash_Screen: Long = 3000 //delay duration for 3 seconds
+    private val Total_Progress: Long = 3000
 
     private val splashRunnable = Runnable{
         val intent = Intent(this@SplashScreen, GetStarted::class.java) //To go to Main Activity or next page.
@@ -21,11 +24,32 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
+        progressBar = findViewById(R.id.Progress_Bar)
+        progressBar.max = 75
+
+        var currentProgress = 0
+        val progressIncrement = 100
+        val progressUpdateInterval = Total_Progress /progressIncrement
+
+        handler.postDelayed(object: Runnable{
+            override fun run() {
+                progressBar.progress = currentProgress
+
+                if(currentProgress < 100){
+                    currentProgress++
+                    handler.postDelayed(this, progressUpdateInterval)
+                }
+                else{
+                    finish()
+                }
+            }
+        }, progressUpdateInterval)
+
         handler.postDelayed(splashRunnable, Delay_Splash_Screen)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacks(splashRunnable) // this removes any pending splashrunnable once activity is destroyed,
+        handler.removeCallbacksAndMessages(null) // this removes any pending splashrunnable once activity is destroyed,
     }
 }
