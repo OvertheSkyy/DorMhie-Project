a96ed4b2798193b0ce10280d8c53a04a4b77b441
BryceMadelo <brycemadelo07@gmail.com>
2023-06-22 15:48:26 +0800

    Create Account Button and Initial Home Page
    When Create Account Button is clicked it will redirect to Create Account page.

---
+++ app/src/main/java/com/example/hobbits/Create_Account.kt
@@ -0,0 +1,11 @@
+package com.example.hobbits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Create_Account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_account)
+    }
+}
--- app/src/main/java/com/example/hobbits/Log_In.kt
+++ app/src/main/java/com/example/hobbits/Log_In.kt
@@ -2,10 +2,17 @@ package com.example.hobbits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class Log_In : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val CreateAccountButton = findViewById<Button>(R.id.CreateAccButton)
+        CreateAccountButton.setOnClickListener {
+            startActivity(Intent(this, Create_Account::class.java)) //this will start Create Account Activity
+        }
     }
 }
