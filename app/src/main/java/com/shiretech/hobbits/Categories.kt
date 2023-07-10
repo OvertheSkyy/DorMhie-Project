12cbdc63aea29c69c33f7cb5a033ab7a05bb35b4
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2023-07-10 08:17:34 +0800

    Fetched categories from Realtime Database
    - updated Categories.kt
    - update categories_subpage.xml

--- app/src/main/java/com/shiretech/hobbits/Categories.kt
+++ app/src/main/java/com/shiretech/hobbits/Categories.kt
@@ -1,15 +1,27 @@
 package com.shiretech.hobbits
 
+import com.google.firebase.database.*
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
 class Categories : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.categories_subpage)
 
+        // Initialize Firebase database reference
+        database = FirebaseDatabase.getInstance().reference
+
+        // Fetch data from Firebase and render it in the text views
+        fetchCategories()
+
+        // Set click listeners for other elements
         val UnselectedHomeImageClick = findViewById<ImageView>(R.id.ClickHome)
         UnselectedHomeImageClick.setOnClickListener {
             val intent = Intent(this, Home::class.java)
@@ -24,6 +36,55 @@ class Categories : AppCompatActivity() {
         UnselectedUserImageClick.setOnClickListener {
             val intent = Intent(this, User_Profile::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun fetchCategories() {
+        val categoriesReference = database.child("categories")
+        categoriesReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val categoryList = ArrayList<String>()
+                    for (categorySnapshot in dataSnapshot.children) {
+                        val category = categorySnapshot.child("name").getValue(String::class.java)
+                        category?.let { categoryList.add(it) }
+                    }
+                    renderCategories(categoryList)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle the error here if needed
+            }
+        })
+    }
+
+    private fun renderCategories(categoryList: List<String>) {
+
+        val firstCategoryTextView = findViewById<TextView>(R.id.FirstCategory)
+        val secondCategoryTextView = findViewById<TextView>(R.id.SecondCategory)
+        val thirdCategoryTextView = findViewById<TextView>(R.id.ThirdCategory)
+        val fourthCategoryTextView = findViewById<TextView>(R.id.FourthCategory)
+        val fifthCategoryTextView = findViewById<TextView>(R.id.FifthCategory)
+        val sixthCategoryTextView = findViewById<TextView>(R.id.SixthCategory)
+
+        if (categoryList.size >= 1) {
+            firstCategoryTextView.text = categoryList[0]
+        }
+        if (categoryList.size >= 2) {
+            secondCategoryTextView.text = categoryList[1]
+        }
+        if (categoryList.size >= 3) {
+            thirdCategoryTextView.text = categoryList[2]
+        }
+        if (categoryList.size >= 4) {
+            fourthCategoryTextView.text = categoryList[3]
+        }
+        if (categoryList.size >= 5) {
+            fifthCategoryTextView.text = categoryList[4]
+        }
+        if (categoryList.size >= 6) {
+            sixthCategoryTextView.text = categoryList[5]
         }
     }
 }
