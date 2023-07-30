fcfe86994f4a9639141bc73f20e5f711b5a834f7
BryceMadelo <brycemadelo07@gmail.com>
2023-07-30 15:28:36 +0800

    Profile Picture Logic

--- app/src/main/java/com/shiretech/hobbits/User_Profile.kt
+++ app/src/main/java/com/shiretech/hobbits/User_Profile.kt
@@ -2,9 +2,17 @@ package com.shiretech.hobbits
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import android.net.Uri
 import android.view.View
 import android.content.Context
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
@@ -15,12 +23,23 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.bumptech.glide.Glide
+import android.content.SharedPreferences
 
 
 class User_Profile : AppCompatActivity() {
+
+    companion object {
+        const val READ_EXTERNAL_STORAGE_REQUEST = 101
+    }
+
+    private lateinit var profileImage: ImageView
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_profile)
+
+        profileImage = findViewById(R.id.ProfileImage)
 
         val UnselectedHomeImageClick = findViewById<ImageView>(R.id.ClickHome)
         UnselectedHomeImageClick.setOnClickListener {
@@ -31,6 +50,35 @@ class User_Profile : AppCompatActivity() {
         UnselectedCategoriesImageClick.setOnClickListener {
             val intent = Intent(this, Categories::class.java)
             startActivity(intent)
+        }
+
+        profileImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Request the permission if not granted
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_EXTERNAL_STORAGE_REQUEST
+                )
+            } else {
+                // Permission already granted, open the gallery
+                openGallery()
+            }
+        }
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        // Retrieve the saved image URI and load it if available
+        val savedImageUriString = sharedPreferences.getString("imageUri", null)
+        if (savedImageUriString != null) {
+            val savedImageUri = Uri.parse(savedImageUriString)
+            Glide.with(this)
+                .load(savedImageUri)
+                .circleCrop()
+                .into(profileImage)
         }
 
         val username = findViewById<TextView>(R.id.UserName)
@@ -100,4 +148,51 @@ class User_Profile : AppCompatActivity() {
                 finish() // Optional: finish the current activity to prevent back navigation
             }
     }
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(galleryIntent)
+
+    }
+    // ActivityResultLauncher to handle gallery intent result
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                // Retrieve the selected image URI from the intent data
+                val imageUri: Uri? = data?.data
+                // Now you can use the selected image URI to display the image using Glide
+                if (imageUri != null) {
+                    Glide.with(this)
+                        .load(imageUri)
+                        .circleCrop() // This will make the image circular
+                        .into(profileImage)// Apply circle crop transformation
+
+                    val editor = sharedPreferences.edit()
+                    editor.putString("imageUri", imageUri.toString())
+                    editor.apply()
+                }
+            }
+        }
+    // Handle permission request result
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_EXTERNAL_STORAGE_REQUEST) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, open the gallery
+                openGallery()
+            } else {
+                // Permission denied, show a toast or handle the case
+                Toast.makeText(
+                    this,
+                    "Storage permission denied. Cannot open gallery.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
 }
